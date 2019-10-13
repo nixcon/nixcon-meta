45772f015ce5e1d1aed8b7d969298702dc5e5d5d
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-10-13 18:16:56 -0400

    nixcon-projector: Allows saving through the electron app

--- streaming-live-media/overlay/nixcon-projector/app/pages/configuration.js
+++ streaming-live-media/overlay/nixcon-projector/app/pages/configuration.js
@@ -3,6 +3,8 @@ import CONFIG from "@configuration";
 
 import Viewer from "./viewer";
 
+const fs = window.require ? window.require("fs") : null;
+
 const Text = ({value, onChange}) =>
 	<input
 		type="text"
@@ -25,6 +27,20 @@ class Configurator extends Component {
 		this.setState({config});
 	}
 
+	get canSave() {
+		return fs !== null;
+	}
+
+	save(e) {
+		e.preventDefault();
+		const {config} = this.state;
+		// Hmmm, this assumes cwd is the location of configuration.json
+		// FIXME: Fix configuration.json as a well known location.
+		fs.writeFileSync("./configuration.json", JSON.stringify(config, null, "  "));
+
+		return false;
+	}
+
 	render() {
 		const {config} = this.state;
 
@@ -34,16 +50,23 @@ class Configurator extends Component {
 					<h1>GFX Configuration</h1>
 				</header>
 				<div className="configuration">
-					<form>
+					<form onSubmit={(e) => this.save(e)}>
 						{
 							["name", "alias", "avatar"].map((name) =>
-								<div>
+								<div key={name}>
 									<label>
 										<span>{name}</span>
 										<Text onChange={(value) => this.setConfig(name, value)} value={config[name]} />
 									</label>
 								</div>
 							)
+						}
+
+						{
+							this.canSave &&
+								<div>
+									<button type="submit">Save</button>
+								</div>
 						}
 					</form>
 				</div>
