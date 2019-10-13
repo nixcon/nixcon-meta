0f906cbd3b0e12c3f755162f7bd2cf7b70421d6f
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-10-13 16:13:13 -0400

    nixcon-projector: Adds WIP configuration page

---
+++ streaming-live-media/overlay/nixcon-projector/app/pages/configuration.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Viewer from "./viewer";
+
+const App = () => <div>
+	<header>
+		<h1>GFX Configuration</h1>
+	</header>
+	<div className="preview">
+		<Viewer />
+	</div>
+</div>;
+
+export default App;
