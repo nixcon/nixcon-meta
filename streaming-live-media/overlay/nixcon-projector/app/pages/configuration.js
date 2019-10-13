fb709c9c8e2d6f423cf3880deff01dba524b2b73
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-10-13 16:50:44 -0400

    nixcon-projector: configuration can be edited, cannot be saved yet

--- streaming-live-media/overlay/nixcon-projector/app/pages/configuration.js
+++ streaming-live-media/overlay/nixcon-projector/app/pages/configuration.js
@@ -1,14 +1,58 @@
-import React from "react";
+import React, {Component} from "react";
+import CONFIG from "@configuration";
 
 import Viewer from "./viewer";
 
-const App = () => <div>
-	<header>
-		<h1>GFX Configuration</h1>
-	</header>
-	<div className="preview">
-		<Viewer />
-	</div>
-</div>;
+const Text = ({value, onChange}) =>
+	<input
+		type="text"
+		value={value}
+		onChange={(e) => onChange(e.target.value)}
+	/>
+;
 
-export default App;
+class Configurator extends Component {
+	constructor() {
+		super();
+		this.state = {
+			config: Object.assign({}, CONFIG),
+		};
+	}
+
+	setConfig(name, value) {
+		const config = Object.assign({}, this.state.config);
+		config[name] = value;
+		this.setState({config});
+	}
+
+	render() {
+		const {config} = this.state;
+
+		return (
+			<div>
+				<header>
+					<h1>GFX Configuration</h1>
+				</header>
+				<div className="configuration">
+					<form>
+						{
+							["name", "alias", "avatar"].map((name) =>
+								<div>
+									<label>
+										<span>{name}</span>
+										<Text onChange={(value) => this.setConfig(name, value)} value={config[name]} />
+									</label>
+								</div>
+							)
+						}
+					</form>
+				</div>
+				<div className="preview">
+					<Viewer />
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Configurator;
