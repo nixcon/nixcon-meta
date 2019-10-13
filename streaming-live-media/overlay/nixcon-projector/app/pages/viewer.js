eefd0dd8b334058bb5d99118baa95c4383522994
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-10-13 16:50:56 -0400

    nixcon-projector: POC viewer reads configuration

--- streaming-live-media/overlay/nixcon-projector/app/pages/viewer.js
+++ streaming-live-media/overlay/nixcon-projector/app/pages/viewer.js
@@ -1,7 +1,11 @@
 import React from "react";
+import config from "@configuration";
 
-const App = () => <div>
-	viewer
-</div>;
+const App = () =>
+	<div id="viewer">
+		<h1>{config["name"]}</h1>
+		<h2>{config["alias"]}</h2>
+	</div>
+	;
 
 export default App;
