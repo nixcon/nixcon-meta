656d88b0a9ae1471cdc99103952596ddf50c92c2
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-10-17 01:03:34 -0400

    nixcon-projector: nixcon-projector: Make callPackageable

--- streaming-live-media/overlay/nixcon-projector/default.nix
+++ streaming-live-media/overlay/nixcon-projector/default.nix
@@ -1,23 +1,18 @@
-let
-  channel = fetchTarball channel:nixos-19.09;
-  dir = toString ./.;
-in
-{
-  pkgs ? import channel {}
-}:
+{ yarn2nix-moretea, electron, makeDesktopItem, ruby }:
 
-pkgs.yarn2nix-moretea.mkYarnPackage {
+yarn2nix-moretea.mkYarnPackage {
   name = "nixcon-projector";
   src = ./.;
   packageJSON = ./package.json;
   yarnLock = ./yarn.lock;
-  # NOTE: this is optional and generated dynamically if omitted
   yarnNix = ./yarn.nix;
 
   postInstall = ''
     substituteInPlace $out/bin/nixcon-projector \
       --subst-var-by project $out/libexec/nixcon2019-gfx/deps/nixcon2019-gfx/ \
-      --subst-var-by electron ${pkgs.electron}/bin/electron
+      --subst-var-by electron ${electron}/bin/electron
 
+    ln -s $out/libexec/nixcon2019-gfx/deps/nixcon2019-gfx/get_talks.rb $out/bin/nixcon-get-talks
+    sed -i '1 i\#!${ruby}/bin/ruby' $out/bin/nixcon-get-talks
   '';
 }
